2a001c587c931e7bab6ae370aa9d536e91e6cfdb
Jeremy Harris <jharris@rockharbor.org>
2011-10-04 09:35:51 -0700

    Initial commit

---
+++ libs/child_theme.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * RH Orange theme
+ * 
+ * @package rockharbor
+ * @subpackage orange
+ */
+class ChildTheme extends RockharborThemeBase {
+		
+/**
+ * List of options for this theme
+ * 
+ * @var array
+ */
+	protected $themeOptions = array(
+		'slug' => 'orange',
+		'short_name' => 'Orange',
+		'supports' => array(
+			'staff'
+		)
+	);
+}
